d40c1692bf42f0e5aca65aeb1140fc686052058f
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-02-14 16:28:05 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "tst-dwefwev3-dev-63511",
   url: "https://tst-dwefwev3-dev-63511.botics.co",
   api: "https://tst-dwefwev3-dev-63511.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled2/index.js
@@ -0,0 +1,31 @@
+import { Pressable } from "react-native";
+import { Text } from "react-native";
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled2 = ({
+  navigation
+}) => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><Pressable onPress={() => navigation.navigate("Untitled3", {})}><Text style={styles.fCZmONcm}>Lorem ipsum…</Text></Pressable></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  fCZmONcm: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
+export default Untitled2;
